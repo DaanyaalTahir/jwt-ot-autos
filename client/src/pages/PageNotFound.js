a245dcbd95501f47b92f5600f0da28b5f8717ea1
Jp <jeanpaul.saliba@ontariotechu.net>
2024-03-06 21:32:16 -0500

    style changes to 403 Page

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,12 +1,34 @@
 import React from "react";
 
 function PageNotFound() {
-    return (
-        <div>
-            <h1>403</h1>
-            <p>Page Not Found</p>
-        </div>
-    );
+  return (
+    <div style={styles.pageNotFound}>
+      <h1 style={styles.heading}>403</h1>
+      <p style={styles.paragraph}>Page Not Found</p>
+    </div>
+  );
 }
+
+const styles = {
+  pageNotFound: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor: "#f44336", // Red background
+    color: "white", // text color
+  },
+  heading: {
+    fontSize: "4rem",
+    marginBottom: "20px",
+    padding: "10px",
+    border: "4px solid black", // border around the text
+    borderRadius: "5px", // rounds the corners
+  },
+  paragraph: {
+    fontSize: "1.5rem", // Medium font size for the paragraph
+  },
+};
 
 export default PageNotFound;
